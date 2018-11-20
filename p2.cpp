0dbc187e41464a3d5de98457d683575f36f9885f
Joseph Sungpil Choi <chousemath@gmail.com>
2018-11-20 23:31:38 +0900

    Quick save

--- p2.cpp
+++ p2.cpp
@@ -1,28 +1,27 @@
 #include <iostream>
 #include <string>
 
-int main()
-{
-    char grade = 'A';
-    std::string phrase = "Joseph is a person";
-    std::string phrase2 = "This is another phrase";
-    if (phrase2.length() > 10)
-    {
-        std::cout << "Man...phrase 2 is real long..." << std::endl;
-    }
-    int age = 120;
-    float temperature = 123.321;
-    double preciseTemperature = 4.12341234;
-    bool isMale = true;
-
-    if (isMale)
-    {
-        std::cout << "It is " << preciseTemperature
-                  << " degrees outside...and I am male" << std::endl;
-    }
-
-    std::cout << phrase << " and he has the grade " << grade << " in class..."
+int main() {
+  char grade = 'A';
+  std::string phrase = "Joseph is a person";
+  std::string phrase2 = "This is another phrase";
+  if (phrase2.length() > 10) {
+    std::cout << "Man...phrase 2 is real long..." << std::endl;
+    std::cout << "And the first character of phrase2 is " << phrase2[0]
               << std::endl;
-    std::cout << "I am great and..." << phrase2 << std::endl;
-    return 0;
+  }
+  int age = 120;
+  float temperature = 123.321;
+  double preciseTemperature = 4.12341234;
+  bool isMale = true;
+
+  if (isMale) {
+    std::cout << "It is " << preciseTemperature
+              << " degrees outside...and I am male" << std::endl;
+  }
+
+  std::cout << phrase << " and he has the grade " << grade << " in class..."
+            << std::endl;
+  std::cout << "I am great and..." << phrase2 << std::endl;
+  return 0;
 }
